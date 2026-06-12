d4bc25eb81a28de914f6161ec5e4f5b0e26df211
Kamlesh Jayasankar <166386342+KamleshJayasankar@users.noreply.github.com>
2026-06-12 22:35:26 +0530

    Time: 0 ms (100%), Space: 19.7 MB (73.15%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -0,0 +1,36 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        first_largest = nums[0]
+        second_largest = None
+        third_largest = None
+        for i in range(1,len(nums)):
+            if nums[i] > first_largest:
+                if second_largest is None:
+                    second_largest = first_largest
+                    first_largest = nums[i]
+                elif second_largest < first_largest and third_largest is None:
+                    third_largest = second_largest
+                    second_largest = first_largest
+                    first_largest = nums[i]
+                elif second_largest < first_largest and third_largest < second_largest:
+                    third_largest = second_largest
+                    second_largest = first_largest
+                    first_largest = nums[i]
+            elif first_largest > nums[i]:
+                if second_largest is None:
+                    second_largest = nums[i]
+                elif second_largest < nums[i] and third_largest is None:
+                    third_largest = second_largest
+                    second_largest = nums[i]
+                elif second_largest < nums[i] and third_largest < second_largest:
+                    third_largest = second_largest
+                    second_largest = nums[i]
+                elif second_largest > nums[i] and third_largest is None:
+                    third_largest = nums[i]
+                elif second_largest > nums[i] and third_largest < nums[i]:
+                    third_largest = nums[i]
+        
+        if third_largest is not None:
+            return third_largest
+        else:
+            return first_largest
