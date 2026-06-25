2f2be3c0d20426bc5ebf541d8062f4afb7f1e027
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-06-25 22:30:37 +0530

    Time: 51 ms (66.3%), Space: 19.8 MB (65.35%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -12,5 +12,6 @@ class Solution:
                 current_count += 1
             if s[i-k] in vowels:
                 current_count -= 1
-            max_count = max(max_count,current_count)
+            if current_count > max_count:
+                max_count = current_count
         return max_count
