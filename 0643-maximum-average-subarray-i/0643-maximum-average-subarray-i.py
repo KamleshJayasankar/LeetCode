00514ac937f525cfbd11d0679628ccd3fea71d0a
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-06-25 20:58:08 +0530

    Time: 55 ms (75.34%), Space: 29.1 MB (49.5%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -3,8 +3,7 @@ class Solution:
         window_slide = sum(nums[0:k])
         max_sum = window_slide
         for i in range(k,len(nums)):
-            window_slide += nums[i]
-            window_slide -= nums[i - k]
+            window_slide = window_slide + nums[i] - nums[i-k]
             max_sum = max(max_sum , window_slide)
 
         return max_sum / k
