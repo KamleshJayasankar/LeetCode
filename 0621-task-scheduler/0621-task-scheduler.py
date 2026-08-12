f11cad59dca3c0686067d3d06424723f2eee2446
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-08-12 21:41:36 +0530

    Time: 14 ms (91.96%), Space: 20.7 MB (84.06%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,13 @@
+import heapq
+from collections import Counter, deque
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        task_counts = Counter(tasks)
+        max_freq = max(task_counts.values())
+
+        max_freq_count = sum(1 for count in task_counts.values() if count == max_freq)
+
+        intervals = (max_freq - 1) * (n + 1) + max_freq_count
+
+        return max(len(tasks), intervals)
