9b62b20a6c08545dcfd86cf0529a68cb6ecab406
Kamlesh Jayasankar <kamleshjayasankar@gmail.com>
2026-09-14 11:04:26 +0530

    Time: 2 ms (64.89%), Space: 47.2 MB (88.2%) - LeetHub

---
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length;
+        int[] result = new int[n];
+        int[] freq = new int[n + 1];
+        int commonCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            freq[A[i]]++;
+            if (freq[A[i]] == 2) {
+                commonCount++;
+            }
+
+            
+            freq[B[i]]++;
+            if (freq[B[i]] == 2) {
+                commonCount++;
+            }
+
+            result[i] = commonCount;
+        }
+
+        return result;
+    }
+}
